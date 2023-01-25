f8c84715a2a2a92ce4a450eaff0653f8536e951d
Your name <dreyer.wil@gmail.com>
2023-01-25 10:12:02 +0100

    app.js

---
+++ frontend/script.js
@@ -0,0 +1,41 @@
+document.querySelector("#searchBtn").addEventListener("click", function () {
+  fetch("http://localhost:3000/trips")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.trips) {
+        for (let i = 0; i < data.trips.length; i++) {
+          const hour = data.trips[i].date.toISOString().substring(12, 17);
+          document.querySelector("#resultCard").innerHTML += `
+                <div class="resultSearch"><span class="tripId" id ="${data.trips[i]._id}" ></span>
+                <span class="departure" >${data.trips[i].departure}</span>
+                <span class="arrival">${data.trips[i].arrival}</span>
+                <span class="hour">${hour}</span>
+                <span class="price">${data.trips[i].price}</span>
+                <button class="bookBtn" id ="${data.trips[i]._id}">Book</button></div>
+			`;
+          bookingBtnListen();
+        }
+      } else {
+        document.querySelector("#resultCard").innerHTML += `
+        <div id ="notripfound">
+        <img src ="/images/notfound.jpg">
+        No trip found
+        </div>
+        `;
+      }
+    });
+});
+
+function bookingBtnListen() {
+  for (let i = 0; i < document.querySelectorAll(".deleteCity").length; i++) {
+    document
+      .querySelectorAll(".bookBtn")
+      .addEventListener("click", function () {
+        fetch(`http://localhost:3000/carts/${this.id}`, { method: "PUT" })
+          .then((response) => response.json())
+          .then((data) => {
+            window.location.assign("cart.html");
+          });
+      });
+  }
+}
