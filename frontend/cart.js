21aa0acd83b06e8f0538e8df7ff2de630d8c4104
Naoëlle <alyiiaah@live.fr>
2023-01-25 17:32:58 +0100

    lastcommit

--- frontend/cart.js
+++ frontend/cart.js
@@ -1,15 +1,14 @@
-fetch("http://localhost:3000/carts", {
-  method: "GET",
-  headers: { "Content-Type": "application/json" },
-})
+fetch("http://localhost:3000/carts", {})
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    document.querySelector(".resultCart").innerHTML = `
-									<div class="resultSearch"><span class="tripId" id ="${data.carts[0]._id}" ></span>
-									<span class="departure" >${data.carts[0].departure} ></span>
-									<span class="arrival">${data.carts[0].arrival}</span>
-									<span class="price">${data.carts[0].price}€</span>
-									<button class="deleteBtn" id ="${data.carts[0]._id}">X</button></div>
+    for (let i = 0; i < data.carts.length; i++) {
+      console.log(data);
+      document.querySelector(".resultCart").innerHTML += `
+									<div class="resultSearch"><span class="tripId" id ="${data.carts[i]._id}" ></span>
+									<span class="departure" >${data.carts[i].departure} ></span>
+									<span class="arrival">${data.carts[i].arrival}</span>
+									<span class="price">${data.carts[i].price}€</span>
+									<button class="deleteBtn" id ="${data.carts[i]._id}">X</button></div>
 						`;
+    }
   });
