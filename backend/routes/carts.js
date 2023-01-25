27cfdc5639360d80d2c2f793acc1d1521fa6dd37
Naoëlle <alyiiaah@live.fr>
2023-01-25 16:09:08 +0100

    ccommit

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -5,19 +5,22 @@ require("../models/connection");
 const Trip = require("../models/trips");
 
 router.get("/", function (req, res, next) {
-    Trip.find({
-       cart: true,   
-    }) .then((data) => { 
-        res.json({ Carts : data } )
-    })
+  Trip.find({
+    cart: true,
+  }).then((data) => {
+    res.json({ carts: data });
+  });
 });
 
 router.put("/:tripID", function (req, res, next) {
-    Trip.updateOne({
-        _id : req.params.tripID },{ cart: true }) .then((data) => { 
-        res.json({ Carts : data } )
-    })
+  Trip.updateOne(
+    {
+      _id: req.params.tripID,
+    },
+    { cart: true }
+  ).then((data) => {
+    res.json({ Carts: data });
+  });
 });
-
 
 module.exports = router;
---
+++ frontend/cart.js
@@ -0,0 +1,15 @@
+fetch("http://localhost:3000/carts", {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    document.querySelector(".resultCart").innerHTML += `
+									<div class="resultSearch"><span class="tripId" id ="${data.carts[0]._id}" ></span>
+									<span class="departure" >${data.carts[0].departure} ></span>
+									<span class="arrival">${data.carts[0].arrival}</span>
+									<span class="price">${data.carts[0].price}€</span>
+									<button class="deleteBtn" id ="${data.carts[0]._id}">X</button></div>
+						`;
+  });
